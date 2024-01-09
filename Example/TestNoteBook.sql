756277e6705e7484cf633f5e346cf495745d805e
joeclarke00 <joeclarke00>
2024-01-09 16:39:18 +0000

    g

--- Example/TestNoteBook.sql
+++ Example/TestNoteBook.sql
@@ -1,3 +1,9 @@
 -- Databricks notebook source
 -- MAGIC %sql
--- MAGIC SELECT "Data Bricks is the best"
+-- MAGIC SELECT 1*4
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC spark_conf = SparkConf().set("spark.driver.extraJavaOptions", "-Dio.netty.tryReflectionSetAccessible=true")
+-- MAGIC spark = SparkSession.builder.config(conf=spark_conf).appName("DatabricksExample").getOrCreate()
